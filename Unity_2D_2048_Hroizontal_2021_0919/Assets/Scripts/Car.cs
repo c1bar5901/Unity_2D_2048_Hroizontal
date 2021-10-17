447b910e8db4f4a4806a8790d6294a5c8a491efe
c1bar5901 <90996456+c1bar5901@users.noreply.github.com>
2021-10-17 13:03:15 +0800

    C#藍圖與導入物件

---
+++ Unity_2D_2048_Hroizontal_2021_0919/Assets/Scripts/Car.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;      //引用    UnityEngine API
+
+//class 類別(藍圖)
+public class Car : MonoBehaviour
+{
+    //欄位 Field
+    //類別 欄位名稱 指定 預設值 結束符號
+    //欄位的四大類別
+    //整數    int     :沒有小數點的數字   預設值0
+    //浮點數  float   :有小數點的數字     預設值0
+    //字串    string  :文字               預設值空
+    //布林值  bool    :是與否 true&false            預設值false
+    int cc = 2000;
+    float weight = 3.5f;
+    string brand = "賓士";
+    bool hasSkywindow = true;  
+}
