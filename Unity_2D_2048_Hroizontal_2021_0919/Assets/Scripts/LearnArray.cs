738bf4d20532534535b3bfbffe9f7c9de945ac4d
c1bar5901 <90996456+c1bar5901@users.noreply.github.com>
2021-11-14 01:43:41 +0800

    API靜態與非靜態

--- Unity_2D_2048_Hroizontal_2021_0919/Assets/Scripts/LearnArray.cs
+++ Unity_2D_2048_Hroizontal_2021_0919/Assets/Scripts/LearnArray.cs
@@ -12,9 +12,31 @@ public class LearnArray : MonoBehaviour
     //資料類型[] - 陣列資料類型
     //陣列：儲存相同資料類型
     public int[] scores;
+    // 定義一個包含數量的陣列
+    public float[] attacks = new float[3];
+    //定義包含值的陣列
+    public string[] props = { "紅水", "藍水","蘋果" };
 
     private void Start()
     {
         score3 = 60;
+
+        //存取陣列資料
+        //取得陣列資料
+        //語法：陣列名稱[編號] - 編號從0開始
+        print("第三個道具："+props[2]);
+        // print("第三個道具：" + props[3]);  //OutOfRange 超出範圍錯誤
+        //設定陣列資料
+        //語法：陣列明超[編號] 指定 值 ;
+        props[0] = "黃水";
+
+        //陣列數量 Length
+        print("道具數量:" + props.Length);
+
+        //利用迴圈設定陣列值 ：10 ~ 50
+        for (int i = 0; i < scores.Length; i++)
+        {
+            scores[i] = i * 10 + 10;
+        }
     }
 }
