314d93ffeb2108191dbca7203461323f7db1d088
c1bar5901 <90996456+c1bar5901@users.noreply.github.com>
2021-11-14 17:07:04 +0800

    遊戲基本功能實作

--- Unity_2D_2048_Hroizontal_2021_0919/Assets/Scripts/MenuManager.cs
+++ Unity_2D_2048_Hroizontal_2021_0919/Assets/Scripts/MenuManager.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 /// <summary>
 /// 開始畫面選單管理器
 /// 開始遊戲、設定、離開遊戲
 /// </summary>
+/// 繼承類別就可以存取其成員：欄位、屬性、方法
 public class MenuManager : MonoBehaviour
 {
     //unity 按鈕與程式溝通
     //1.公開方法
     //2.按鈕設定點擊事件 on Click
 
+    public AudioMixer mixer;
+
     /// <summary>
     /// 開始遊戲
     /// </summary>
-    public void StartGame()
+    public void StartGame (float delay)
+    {
+        //使用繼承類別的成員語法：
+        //繼承類別的方法
+        //方法名稱(對應的引數)
+        //延遲 delay 秒後呼叫 方法
+        Invoke("DelayStartGame", delay);
+    }
+    public void DelayStartGame()
     {
         // 場景管理，載入場警(場景名稱)
         SceneManager.LoadScene("遊戲場景");
@@ -24,19 +36,30 @@ public class MenuManager : MonoBehaviour
     /// <summary>
     /// 設定遊戲
     /// </summary>
-    public void SettingGame()
+    public void SettingGameBGM(float volume)
     {
-
+        print("BGM 滑桿音量：" + volume);
+        mixer.SetFloat("音量BGM", volume);
+        
     }
-
+    public void SettingGameSFX(float volume)
+    {
+        print("SFX 滑桿音量：" + volume);
+        mixer.SetFloat("音量SFX", volume);
+    }
     /// <summary>
     /// 離開遊戲
     /// </summary>
-    public void QuitGame()
+    public void QuitGame(float delay)
+    {
+        Invoke("DelayQuitGame", delay);
+    }
+    private void DelayQuitGame()
     {
         //應用程式.離開();
         //Quit 不會再 Editor 執行，發布執行黨、手機、PC
         Application.Quit();
+
         print("離開遊戲");
     }
 }
